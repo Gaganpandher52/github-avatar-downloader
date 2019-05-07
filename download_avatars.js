11f4d965134b9df3f1f26071ca83130a8e36219c
Gaganpandher52 <gaganpandher52@gmail.com>
2019-05-07 02:02:21 +0000

    parse working

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,30 @@
 var request = require('request');
+var secretsFile = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-    request(url, function(err, res, body) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': secretsFile.GITHUB_TOKEN
+      }
+    };
+    
+    request(options, function(err, res, body) {
+        var repos = JSON.parse(body);
+        repos.forEach(function(repo) {
+            
+            console.log(repo.avatar_url);
+            
+          });
       cb(err, body);
     });
   }
-
+ 
   getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+    //console.log("Errors:", err);
+    //console.log("Result:", result);
   });
  
